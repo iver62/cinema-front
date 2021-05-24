8619b0e304b11a80864bf5f3958b25f353a24c33
Pierrick <deshayes.pierrick@hotmail.fr>
2021-05-24 21:09:24 +0200

    migration to angular 12

--- src/app/views/countries/countries.component.spec.ts
+++ src/app/views/countries/countries.component.spec.ts
@@ -1,11 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { CountriesComponent } from './countries.component';
 
 describe('CountriesComponent', () => {
   let component: CountriesComponent;
   let fixture: ComponentFixture<CountriesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [CountriesComponent]
     })
